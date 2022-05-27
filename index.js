c1aec85a2eca637b8de0c4158b194c1b6bdc5514
lailalf <lailaluciodev@outlook.com>
2022-05-26 22:12:30 -0300

    Trabalhando com Rotas

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const http = require('http');
-
-const hostname = '127.0.0.1'; 
-const port = 3000; 
-
-const server = http.createServer((req,res) => {
-    res.statusCode= 200;
-    res.setHeader ('Content-Type', 'text/plain');
-    res.end ('Olá mundo Ubá');
-}); 
-
-server.listen(port,hostname, () => {
-console.log ( `Servidor rodando em http://${hostname}:${port}/` );
+const express=require('express');// determinando a utilização do express
+const app=express();
+const path=require ('path');//enderço de cada rota
+const router=express.Router();// trabalha com as rotas
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+router.get('/about',function(req,res){
+    res.sendFile(path.join(__dirname+'/about.html'));
 })
 
-    
+app.use('/',router);
+app.use('/about',router);
+app.listen(process.env.port||3000);
+console.log("Servidor rodando");

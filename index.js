74a953171443f239eee415f4e7956710bc1d9620
lailalf <lailaluciodev@outlook.com>
2022-05-27 19:58:02 -0300

    Criando página com handlebars

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express=require('express');// determinando a utilização do express
 const app=express();
-const path=require ('path');//enderço de cada rota
+const path=require ('path');//endereço de cada rota
 const router=express.Router();// trabalha com as rotas
+
+const {engine} = require ("express-handlebars"); 
+
+app.engine('handlebars', engine({
+    defaultLayout:'main', 
+    runtimeOptions: {
+        allowedProtoPropertiesByDefault:true,
+        allowProtoMethodsByDefault: true,
+    },
+}))
+
+app.set('view engine', 'handlebars');
+
+
+
 router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.render('index');
 })
 router.get('/about',function(req,res){
     res.sendFile(path.join(__dirname+'/about.html'));
@@ -13,3 +28,5 @@ app.use('/',router);
 app.use('/about',router);
 app.listen(process.env.port||3000);
 console.log("Servidor rodando");
+
+
